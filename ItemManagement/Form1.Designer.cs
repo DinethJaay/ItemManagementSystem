19f23d2b6c93b6fed9f640bed4a8d781880bc800
Dineth Jayanga <125632757+DinethJaay@users.noreply.github.com>
2024-12-28 11:36:07 +0530

    Add project files.

---
+++ ItemManagement/Form1.Designer.cs
@@ -0,0 +1,219 @@
+﻿namespace ItemManagement
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            label1 = new Label();
+            label2 = new Label();
+            txtId = new TextBox();
+            txtName = new TextBox();
+            txtPrice = new TextBox();
+            label3 = new Label();
+            btnSave = new Button();
+            btnClear = new Button();
+            label4 = new Label();
+            itemGridView = new DataGridView();
+            label5 = new Label();
+            btnUpdate = new Button();
+            ((System.ComponentModel.ISupportInitialize)itemGridView).BeginInit();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.BackColor = Color.Transparent;
+            label1.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label1.Location = new Point(23, 103);
+            label1.Name = "label1";
+            label1.Size = new Size(30, 28);
+            label1.TabIndex = 0;
+            label1.Text = "Id";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.BackColor = Color.Transparent;
+            label2.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label2.Location = new Point(23, 173);
+            label2.Name = "label2";
+            label2.Size = new Size(68, 28);
+            label2.TabIndex = 1;
+            label2.Text = "Name";
+            // 
+            // txtId
+            // 
+            txtId.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            txtId.Location = new Point(140, 103);
+            txtId.Name = "txtId";
+            txtId.Size = new Size(206, 34);
+            txtId.TabIndex = 2;
+            // 
+            // txtName
+            // 
+            txtName.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            txtName.Location = new Point(140, 173);
+            txtName.Name = "txtName";
+            txtName.Size = new Size(206, 34);
+            txtName.TabIndex = 3;
+            // 
+            // txtPrice
+            // 
+            txtPrice.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            txtPrice.Location = new Point(140, 248);
+            txtPrice.Name = "txtPrice";
+            txtPrice.Size = new Size(206, 34);
+            txtPrice.TabIndex = 5;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.BackColor = Color.Transparent;
+            label3.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label3.Location = new Point(23, 248);
+            label3.Name = "label3";
+            label3.Size = new Size(59, 28);
+            label3.TabIndex = 4;
+            label3.Text = "Price";
+            // 
+            // btnSave
+            // 
+            btnSave.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            btnSave.Location = new Point(23, 331);
+            btnSave.Name = "btnSave";
+            btnSave.Size = new Size(94, 29);
+            btnSave.TabIndex = 6;
+            btnSave.Text = "Save";
+            btnSave.UseVisualStyleBackColor = true;
+            btnSave.Click += btnSave_Click;
+            // 
+            // btnClear
+            // 
+            btnClear.BackColor = SystemColors.ButtonHighlight;
+            btnClear.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            btnClear.Location = new Point(252, 331);
+            btnClear.Name = "btnClear";
+            btnClear.Size = new Size(94, 29);
+            btnClear.TabIndex = 7;
+            btnClear.Text = "Clear";
+            btnClear.UseVisualStyleBackColor = false;
+            btnClear.Click += btnClear_Click;
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.BackColor = Color.Transparent;
+            label4.Font = new Font("Segoe UI", 16.2F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label4.Location = new Point(280, 9);
+            label4.Name = "label4";
+            label4.Size = new Size(261, 38);
+            label4.TabIndex = 8;
+            label4.Text = "Item Management";
+            // 
+            // itemGridView
+            // 
+            itemGridView.BackgroundColor = SystemColors.Control;
+            itemGridView.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            itemGridView.Location = new Point(375, 103);
+            itemGridView.Name = "itemGridView";
+            itemGridView.RowHeadersWidth = 51;
+            itemGridView.Size = new Size(402, 257);
+            itemGridView.TabIndex = 9;
+            itemGridView.CellClick += itemGridView_CellClick;
+            itemGridView.CellContentClick += itemGridView_CellContentClick;
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.BackColor = Color.Transparent;
+            label5.Font = new Font("Segoe UI", 7.8F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label5.Location = new Point(332, 424);
+            label5.Name = "label5";
+            label5.Size = new Size(105, 17);
+            label5.TabIndex = 10;
+            label5.Text = "Designed by DJ";
+            label5.Click += label5_Click;
+            // 
+            // btnUpdate
+            // 
+            btnUpdate.BackColor = SystemColors.ButtonHighlight;
+            btnUpdate.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            btnUpdate.Location = new Point(140, 331);
+            btnUpdate.Name = "btnUpdate";
+            btnUpdate.Size = new Size(94, 29);
+            btnUpdate.TabIndex = 11;
+            btnUpdate.Text = "Update";
+            btnUpdate.UseVisualStyleBackColor = false;
+            btnUpdate.Click += btnUpdate_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = SystemColors.ControlLight;
+            BackgroundImage = (Image)resources.GetObject("$this.BackgroundImage");
+            BackgroundImageLayout = ImageLayout.Zoom;
+            ClientSize = new Size(800, 450);
+            Controls.Add(btnUpdate);
+            Controls.Add(label5);
+            Controls.Add(itemGridView);
+            Controls.Add(label4);
+            Controls.Add(btnClear);
+            Controls.Add(btnSave);
+            Controls.Add(txtPrice);
+            Controls.Add(label3);
+            Controls.Add(txtName);
+            Controls.Add(txtId);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            DoubleBuffered = true;
+            MaximizeBox = false;
+            Name = "Form1";
+            Text = "Item Management";
+            Load += Form1_Load;
+            ((System.ComponentModel.ISupportInitialize)itemGridView).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private Label label2;
+        private TextBox txtId;
+        private TextBox txtName;
+        private TextBox txtPrice;
+        private Label label3;
+        private Button btnSave;
+        private Button btnClear;
+        private Label label4;
+        private DataGridView itemGridView;
+        private Label label5;
+        private Button btnUpdate;
+    }
+}
---
+++ ItemManagement/Form1.cs
@@ -0,0 +1,139 @@
+using Microsoft.Data.SqlClient;
+using System.Data;
+
+namespace ItemManagement
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            var connectionString = "Server=MSI\\SQLEXPRESS;Database=ItemManagement;Trusted_Connection=True;TrustServerCertificate=True;";
+
+            var query = "INSERT INTO Item(name,price) Values(@name,@price)";
+
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                try
+                {
+                    connection.Open();
+                    //MessageBox.Show("Database connected Successfully");
+                    using (SqlCommand cmd = new SqlCommand(query, connection))
+                    {
+                        cmd.Parameters.AddWithValue("@name", txtName.Text);
+                        cmd.Parameters.AddWithValue("@price", Convert.ToDecimal(txtPrice.Text));
+
+                        int rowsAffected = cmd.ExecuteNonQuery();
+                        MessageBox.Show(rowsAffected > 0 ? "Item Added Successfully !" : "Failed to add item.");
+                        DisplayItem();
+                        ClearValues();
+
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+            }
+        }
+
+        private void itemGridView_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            DisplayItem();
+        }
+        private void DisplayItem()
+        {
+            string connectionString = "Server=MSI\\SQLEXPRESS;Database=ItemManagement;Trusted_Connection=True;TrustServerCertificate=True;";
+
+            string query = "Select * from Item";
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                try
+                {
+                    connection.Open();
+                    SqlDataAdapter adapter = new SqlDataAdapter(query, connection);
+
+                    DataTable dt = new DataTable();
+                    adapter.Fill(dt);
+                    itemGridView.DataSource = dt;
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show($"Error: {ex.Message}");
+                }
+            }
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            ClearValues();
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void ClearValues()
+        {
+            txtName.Text = "";
+            txtPrice.Text = "";
+            txtId.Text = "";
+        }
+
+        private void itemGridView_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            if (e.RowIndex >= 0)
+            {
+                //Get Selected Row
+                DataGridViewRow selectedRow = itemGridView.Rows[e.RowIndex];
+
+                //Display selected rows Data in textbox
+                txtId.Text = selectedRow.Cells["Id"].Value.ToString();
+                txtName.Text = selectedRow.Cells["Name"].Value.ToString();
+                txtPrice.Text = selectedRow.Cells["Price"].Value.ToString();
+
+
+
+            }
+        }
+
+        private void btnUpdate_Click(object sender, EventArgs e)
+        {
+            var id = Convert.ToInt32(txtId.Text);
+            var query = "UPDATE Item SET name=@name, price=@price WHERE Id=@id";
+            var connectionString = "Server=MSI\\SQLEXPRESS;Database=ItemManagement;Trusted_Connection=True;TrustServerCertificate=True;";
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                try
+                {
+                    connection.Open();
+                    using (SqlCommand cmd = new SqlCommand(query, connection))
+                    {
+                        cmd.Parameters.AddWithValue("@id", id);
+                        cmd.Parameters.AddWithValue("@name", txtName.Text);
+                        cmd.Parameters.AddWithValue("@price", Convert.ToDecimal(txtPrice.Text));
+
+                        int rowsAffected = cmd.ExecuteNonQuery();
+                        MessageBox.Show(rowsAffected > 0 ? "Item Updated Successfully !" : "Failed to update item.");
+                        DisplayItem();
+                        ClearValues();
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+            }
+        }
+    }
+}
